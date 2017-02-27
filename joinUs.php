b456c5db4fef6ba626cea4aceffb0d96844b4d7a
Matt McGuiness <mattjmcguiness@gmail.com>
2017-02-27 16:32:37 -0500

    Form Completion
    Use regular expressions to check the validity of the form and also added a feature where the form does not reset if you mess up filling it out.

--- joinUs.php
+++ joinUs.php
@@ -57,28 +57,31 @@
 							<form method="post" action="joinUs.php">
 										<div class="row uniform">
 											<div class="12u$">
-												<input type="text" name="first-name" id="first-name" value="" placeholder="First Name" />
+												<input type="text" name="first-name" id="first-name" value="<?php echo isset($_POST['first-name']) ? $_POST['first-name'] : '' ?>" placeholder="First Name" />
 											</div>
 											<div class="12u$">
-												<input type="text" name="last-name" id="last-name" value="" placeholder="Last Name" />
+												<input type="text" name="last-name" id="last-name" value="<?php echo isset($_POST['last-name']) ? $_POST['last-name'] : '' ?>" placeholder="Last Name" />
 											</div>
 											<div class="12u$">
-										        <input type="text" name="username" id="username" value="" placeholder="Username" />
+										        <input type="text" name="username" id="username" value="<?php echo isset($_POST['username']) ? $_POST['username'] : '' ?>" placeholder="Username" />
 											</div>
 											<div class="12u$">
-												<input type="email" name="email" id="email" value="" placeholder="Email" />
+												<input type="password" name="password" id="password" value="<?php echo isset($_POST['password']) ? $_POST['password'] : '' ?>" placeholder="Password" />
 											</div>
 											<div class="12u$">
-												<input type="text" name="address" id="address" value="" placeholder="Street Address" />
+												<input type="email" name="email" id="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>" placeholder="Email" />
 											</div>
 											<div class="12u$">
-												<input type="text" name="city" id="city" value="" placeholder="City" />
+												<input type="text" name="address" id="address" value="<?php echo isset($_POST['address']) ? $_POST['address'] : '' ?>" placeholder="Street Address" />
+											</div>
+											<div class="12u$">
+												<input type="text" name="city" id="city" value="<?php echo isset($_POST['city']) ? $_POST['city'] : '' ?>" placeholder="City" />
 											</div>
 											<div class="6u 12u$(xsmall)">
-												<input type="text" name="state" id="state" value="" placeholder="State" />
+												<input type="text" name="state" id="state" value="<?php echo isset($_POST['state']) ? $_POST['state'] : '' ?>" placeholder="State" />
 											</div>
 											<div class="6u 12u$(xsmall)">
-												<input type="text" name="zip-code" id="zip-code" value="" placeholder="Zip Code" />
+												<input type="text" name="zip-code" id="zip-code" value="<?php echo isset($_POST['zip-code']) ? $_POST['zip-code'] : '' ?>" placeholder="Zip Code" />
 											</div>
 											<div class="12u$">
 												<input type="checkbox" id="agreement" name="agreement">
@@ -95,37 +98,64 @@
 			</section>
 		</article>
 <?php
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 	$username = mysql_real_escape_string($_POST['username']);
-	$firstname = mysql_real_escape_string($_POST['firstname']);
-	$lastname = mysql_real_escape_string($_POST['lastname']);
+	$firstname = mysql_real_escape_string($_POST['first-name']);
+	$lastname = mysql_real_escape_string($_POST['last-name']);
 	$password = mysql_real_escape_string($_POST['password']);
 	$address = mysql_real_escape_string($_POST['address']);
 	$email = mysql_real_escape_string($_POST['email']);
 	$city = mysql_real_escape_string($_POST['city']);
 	$state = mysql_real_escape_string($_POST['state']);
-	$zipcode = mysql_real_escape_string($_POST['zipcode']);
-    $bool = true;
-	mysql_connect("localhost", "root") or die(mysql_error()); //Connect to server
-	mysql_select_db("FirstDataBase") or die("Cannot connect to database"); //Connect to database
-	$query = mysql_query("Select * from userinfo"); //Query the users table
-	while($row = mysql_fetch_array($query)) //display all rows from query
-	{
-		$table_users = $row['username']; // the first username row is passed on to $table_users, and so on until the query is finished
-		if($username == $table_users) // checks if there are any matching fields
-		{
-			$bool = false; // sets bool to false
-			Print '<script>alert("Username has been taken!");</script>'; //Prompts the user
-			Print '<script>window.location.assign("joinUs.php");</script>'; // redirects to joinUs.php
+	$zipcode = mysql_real_escape_string($_POST['zip-code']);
+	$agreement = mysql_real_escape_string($_POST['agreement']);
+	$bool = true;
+
+	if ($username == NULL || $firstname == NULL || 
+		$lastname == NULL || $password == NULL || 
+		$address == NULL || $email == NULL || 
+		$city == NULL || $state == NULL ||
+		$zipcode == NULL || $agreement == NULL) {
+			Print '<script>alert("You must fill out the form completely!");</script>'; // Prompts the user
+		} else {
+
+	
+			if (1 === preg_match('~[0-9]~', $firstname) || 
+				1 === preg_match('~[0-9]~', $lastname)){
+				Print '<script>alert("Names should not contain numbers!");</script>'; // Prompts the user
+			} else {
+				if (1 != preg_match('~[.]~', $email)) {
+					Print '<script>alert("Email must contain at least 1 period!");</script>'; // Prompts the user
+				} else {
+					if ( 1 != preg_match('/^[0-9]{5}$/', $zipcode) && strlen($zipcode) != 5) {
+						Print '<script>alert("Must input a valid zipcode!");</script>'; // Prompts the user
+					} else {
+
+						mysql_connect("localhost", "root") or die(mysql_error()); //Connect to server
+						mysql_select_db("FirstDataBase") or die("Cannot connect to database"); //Connect to database
+						$query = mysql_query("Select * from userinfo"); //Query the users table
+						while($row = mysql_fetch_array($query)) //display all rows from query
+						{
+							$table_users = $row['username']; // the first username row is passed on to $table_users, and so on until the query is finished
+							if($username == $table_users) // checks if there are any matching fields
+							{
+								$bool = false; // sets bool to false
+								Print '<script>alert("Username has been taken!");</script>'; //Prompts the user
+								//Print '<script>window.location.assign("joinUs.php");</script>'; // redirects to joinUs.php
+							}
+						}
+						if($bool) // checks if bool is true
+						{
+							mysql_query("INSERT INTO userinfo(firstname, lastname, username, password, email, address, city, state, zipcode) VALUES ('$firstname','$lastname','$username','$password','$email','$address','$city','$state','$zipcode')"); //Inserts the value to table users
+							
+							Print '<script>alert("Successfully joined!");</script>'; // Prompts the user
+							Print '<script>window.location.assign("joinUs.php");</script>'; // redirects to joinUs.php
+						}
+					}
+				}
+			}
 		}
-	}
-	if($bool) // checks if bool is true
-	{
-		mysql_query("INSERT INTO userinfo(firstname, lastname, username, password, email, address, city, state, zipcode) VALUES ('$firstname','$lastname','$username','$password','$email','$address','$city','$state','$zipcode')"); //Inserts the value to table users
-		
-		Print '<script>alert("Successfully joined!");</script>'; // Prompts the user
-		Print '<script>window.location.assign("joinUs.php");</script>'; // redirects to joinUs.php
-	}
 }
 ?>
 
